77a90b2a425e0959df7072c5641e8c5403f0e1e5
Zach Melamed <zsm767@gmail.com>
2019-11-11 20:27:31 -0800

    11/11 update - finishing NameGenerator v1
    Some initial name lists added in for all available races. Not overly
    extensive, but it'll work. Tested with the bottom test cases, no errors
    found. Updating, and continuing to work on the next part.

--- NameGenerator.py
+++ NameGenerator.py
@@ -13,19 +13,19 @@ class NameGenerator:
 	# for races
 	#----------------------------------------------------------------#
 
-	human_firstnames = []
-	human_lastnames = []
+	human_firstnames = ['Gauwilh', 'Eglod', 'Gerey', 'Hilda', 'Adin', 'Rere', 'Eleth ', 'Helman', 'Atell', 'Freyny',]
+	human_lastnames = ['Velley', 'Shile', 'Gysby', 'Vissio', 'Falconsflight', 'Carter', 'Trannyth', 'Shieldheart', ]
 
 	#----------------------------------------------------------------#
 
 	elf_firstnames = ['Aust', 'Adra', 'Berrian', 'Birel', 'Carric', 'Caelynn', 'Erevan', 'Felosial', 'Ivellion', 'Quarion', 'Lia',
-	'Varis', 'Xanathi']
-	elf_lastnames = ['Amakiir', 'Amastacia', 'Galanodel', 'Holion', 'Liadon', 'Meliamne', 'Siannodel', 'Xiloscient']
+	'Varis', 'Xanathi',]
+	elf_lastnames = ['Amakiir', 'Amastacia', 'Galanodel', 'Holion', 'Liadon', 'Meliamne', 'Siannodel', 'Xiloscient',]
 
 	#----------------------------------------------------------------#
 
-	halfling_firstnames = []
-	halfling_lastnames = []
+	halfling_firstnames = ['Symund', 'Hileon', 'Artis', 'Herim', 'Richye', 'Suse', 'Phily', 'Peona', 'Rosa', 'Gela', 'Mara',]
+	halfling_lastnames = ['Folcey', 'Burrow', 'Ophirn', 'Mere', 'Mere', 'Wellsor',]
 	
 	#----------------------------------------------------------------#
 
@@ -35,18 +35,18 @@ class NameGenerator:
 
 	#----------------------------------------------------------------#
 
-	halforc_firstnames = []
-	halforc_lastnames = []
+	halforc_firstnames = ['Elgold', 'Davkul', 'Wilros', 'Uaerris', 'Quiora', 'Seahand', 'Magkain', 'Quikain', 'Sadasaadi', 'Olofalcon',]
+	halforc_lastnames = ['Fiedlerson', 'Wyvernjack', 'Windsailor', 'Pegason', 'Arroway', 'Trannyth', 'Waveharp',]
 
 	#----------------------------------------------------------------#
 
-	halfelf_firstnames = []
-	halfelf_lastnames = []
+	halfelf_firstnames = ['Coratis', 'Horven', 'Gurstina', 'Stoora', 'Arathana', 'Arahana', 'Ianmorn', 'Yenrry', 'Belben', ]
+	halfelf_lastnames = ['Wolfsbane', 'Joysword', 'Dawntracker', 'Gentleharp', 'Shipsail', 'Darkeyes', 'Chorster', 'Hollysword',]
 
 	#----------------------------------------------------------------#
 
-	tiefling_firstnames = []
-	tiefling_lastnames = []
+	tiefling_firstnames = ['Azza', 'Ea', 'Ishte', 'Aym', 'Manea', 'Markosian', 'Ahrim', 'Iados', 'Andram', 'Amnon', 'Xappan', 'Zephan',]
+	tiefling_lastnames = ['Remorse', 'Belief', 'Aid', 'Talent', 'Fealty', 'Revulsion', 'Violence', 'Scorn', 'Creed', 'Power', 'Haste',]
 
 	#----------------------------------------------------------------#
 
@@ -71,39 +71,39 @@ class NameGenerator:
 			the rFlag works, the first/last tuple for names works. 
 			TODO: look for a more graceful way of handling this outside of a bunch of if's 
 		'''
-		if self.rFlag == 'Hu':
-			first = self.generic_firstnames[randint(0, len(self.human_firstnames) - 1)]
-			last = self.generic_lastnames[randint(0, len(self.human_lastnames) - 1)]
+		if self.rFlag == 'HU':
+			first = self.human_firstnames[randint(0, len(self.human_firstnames) - 1)]
+			last = self.human_lastnames[randint(0, len(self.human_lastnames) - 1)]
 			return first, last
 			
 		if self.rFlag == 'EL':
-			first = self.generic_firstnames[randint(0, len(self.elf_firstnames) - 1)]
-			last = self.generic_lastnames[randint(0, len(self.elf_lastnames) - 1)]
+			first = self.elf_firstnames[randint(0, len(self.elf_firstnames) - 1)]
+			last = self.elf_lastnames[randint(0, len(self.elf_lastnames) - 1)]
 			return first, last
 			
 		if self.rFlag == 'HL':
-			first = self.generic_firstnames[randint(0, len(self.halfling_firstnames) - 1)]
-			last = self.generic_lastnames[randint(0, len(self.halfling_lastnames) - 1)]
+			first = self.halfling_firstnames[randint(0, len(self.halfling_firstnames) - 1)]
+			last = self.halfling_lastnames[randint(0, len(self.halfling_lastnames) - 1)]
 			return first, last
 			
 		if self.rFlag == 'DW':
-			first = self.generic_firstnames[randint(0, len(self.dwarf_firstnames) - 1)]
-			last = self.generic_lastnames[randint(0, len(self.dwarf_lastnames) - 1)]
+			first = self.dwarf_firstnames[randint(0, len(self.dwarf_firstnames) - 1)]
+			last = self.dwarf_lastnames[randint(0, len(self.dwarf_lastnames) - 1)]
 			return first, last
 			
 		if self.rFlag == 'HO':
-			first = self.generic_firstnames[randint(0, len(self.halforc_firstnames) - 1)]
-			last = self.generic_lastnames[randint(0, len(self.halforc_lastnames) - 1)]
+			first = self.halforc_firstnames[randint(0, len(self.halforc_firstnames) - 1)]
+			last = self.halforc_lastnames[randint(0, len(self.halforc_lastnames) - 1)]
 			return first, last
 			
 		if self.rFlag == 'HE':
-			first = self.generic_firstnames[randint(0, len(self.halfelf_firstnames) - 1)]
-			last = self.generic_lastnames[randint(0, len(self.halfelf_lastnames) - 1)]
+			first = self.halfelf_firstnames[randint(0, len(self.halfelf_firstnames) - 1)]
+			last = self.halfelf_lastnames[randint(0, len(self.halfelf_lastnames) - 1)]
 			return first, last
 			
 		if self.rFlag == 'TL':
-			first = self.generic_firstnames[randint(0, len(self.tiefling_firstnames) - 1)]
-			last = self.generic_lastnames[randint(0, len(self.tiefling_lastnames) - 1)]
+			first = self.tiefling_firstnames[randint(0, len(self.tiefling_firstnames) - 1)]
+			last = self.tiefling_lastnames[randint(0, len(self.tiefling_lastnames) - 1)]
 			return first, last
 	
 
@@ -115,10 +115,26 @@ class NameGenerator:
 
 # end class
 
-''' Test Case Block, blocking them out once confirmed to work. '''
-ng = NameGenerator('G')
+''' 
+Test Case Block, blocking them out once confirmed to work. 
+
+ng = NameGenerator('HU')
+ng2 = NameGenerator('EL')
+ng3 = NameGenerator('HL')
+ng4 = NameGenerator('DW')
+ng5 = NameGenerator('HO')
+ng6 = NameGenerator('HE')
+ng7 = NameGenerator('TL')
+
 i = 0
 
 while i < 10:
 	print(ng.getName())
+	print(ng2.getName())
+	print(ng3.getName())
+	print(ng4.getName())
+	print(ng5.getName())
+	print(ng6.getName())
+	print(ng7.getName())
 	i += 1
+```
